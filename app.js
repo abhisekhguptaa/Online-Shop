69e7af410867c9ff239dc5f8086ceaf494d5c6a9
Abhisekh Gupta <abhisekhgupta405@gmail.com>
2023-03-30 20:22:04 +0530

    Earlier pug and handlebar template engines were used, now updated to ejs

--- app.js
+++ app.js
@@ -1,14 +1,14 @@
-const express = require("express"); // import the express module 
+const express = require("express"); // import the express module
 const bodyParser = require("body-parser"); // import the body-parser module
 const path = require("path"); // import the path module
-const expressHbs = require("express-handlebars"); // import the express-handlebars module
+//const expressHbs = require("express-handlebars"); // import the express-handlebars module
 
-const adminData = require("./routes/admin"); // import the admin routes  
-const shopRoutes = require("./routes/shop"); // import the shop routes 
+const adminData = require("./routes/admin"); // import the admin routes
+const shopRoutes = require("./routes/shop"); // import the shop routes
 
-const app = express(); // create an express app 
+const app = express(); // create an express app
 
-app.engine(
+/*app.engine(
   // set the view engine to handlebars and set the extension to 
   "hbs", // set the extension to hbs
   expressHbs({ 
@@ -17,20 +17,25 @@ app.engine(
     defaultLayout: "main-layout", // set the default layout to main-layout 
     extname: "hbs", // set the extension to hbs 
   })
-);
-app.set("view engine", "hbs"); // set the view engine to handlebars 
-//app.set('view engine', 'pug'); // set the view engine to pug 
-app.set("views", "views"); // set the views directory to views 
+);*/
 
-app.use(bodyParser.urlencoded({ extended: false })); // use the body-parser module 
-app.use(express.static(path.join(__dirname, "public"))); // use the public directory 
+//app.set("view engine", "hbs"); // set the view engine to handlebars
+//app.set('view engine', 'pug'); // set the view engine to pug
+app.set("view engine", "ejs"); // set the view engine to ejs
+app.set("views", "views"); // set the views directory to views
 
-app.use("/admin", adminData.routes); // use the admin routes 
-app.use(shopRoutes); // use the shop routes 
+app.use(bodyParser.urlencoded({ extended: false })); // use the body-parser module
+app.use(express.static(path.join(__dirname, "public"))); // use the public directory
 
-app.use((req, res, next) => { 
-  // use the 404 page 
-  res.status(404).render("404", { pageTitle: "Page not found" }); // render the 404 page (handlebars template+pug template) 
+app.use("/admin", adminData.routes); // use the admin routes
+app.use(shopRoutes); // use the shop routes
+
+app.use((req, res, next) => {
+  // use the 404 page
+  res.status(404).render("404", {
+    pageTitle: "Page not found", // pass the page title to the 404 page
+    path: "/404", // pass the path to the 404 page
+  }); // render the 404 page
 });
 
-app.listen(3000); // listen on port 3000 
+app.listen(3000); // listen on port 3000
--- routes/admin.js
+++ routes/admin.js
@@ -1,19 +1,20 @@
 const express = require("express"); // import the express module
 const router = express.Router(); // create a router object from the express module
 const bodyParser = require("body-parser"); // import the body-parser module
-const path = require("path"); // import the path module 
+const path = require("path"); // import the path module
 
-const rootDir = require("../util/path"); // import the path module to the main file 
+const rootDir = require("../util/path"); // import the path module to the main file
 
-const products = []; // create an empty array to store the products in it (it'll be exported to the admin routes) 
+const products = []; // create an empty array to store the products in it (it'll be exported to the admin routes)
 
 router.use(bodyParser.urlencoded({ extended: false })); // use the body-parser module to parse the request body
 
-//itll be fired when the user visits /admin/add-product (GET request) 
-router.get("/add-product", (req, res, next) => { 
-  res.render("add-product", { // render the add-product page (handlebars template)
+//itll be fired when the user visits /admin/add-product (GET request)
+router.get("/add-product", (req, res, next) => {
+  res.render("add-product", {
+    // render the add-product page (handlebars template)
     pageTitle: "Add product", // pass the page title to the add-product page
-    path: "/admin/add-product", // pass the path to the add-product page 
+    path: "/admin/add-product", // pass the path to the add-product page
     formsCSS: true, // pass the formsCSS variable to the add-product page
     productCSS: true, // pass the productCSS variable to the add-product page
     activeAddProduct: true, // pass the activeAddProduct variable to the add-product page
@@ -21,10 +22,10 @@ router.get("/add-product", (req, res, next) => {
 });
 
 //it'll be fired when the form is submitted (POST request)
-router.post("/add-product", (req, res, next) => { 
-  products.push({ title: req.body.title }); // push the product to the products array 
-  res.redirect("/"); // redirect to the home page 
+router.post("/add-product", (req, res, next) => {
+  products.push({ title: req.body.title }); // push the product to the products array
+  res.redirect("/"); // redirect to the home page
 });
 
-exports.routes = router; // export the router object 
-exports.products = products; // export the products array 
+exports.routes = router; // export the router object
+exports.products = products; // export the products array
